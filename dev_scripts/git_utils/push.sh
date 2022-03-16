d30592b893b5e5554ee448b008d37dcab0f3b132
Thonifho Muhali <tmuhali@student.wethinkcode.co.za>
2022-03-16 19:39:47 +0000

    wefrt

--- dev_scripts/git_utils/push.sh
+++ dev_scripts/git_utils/push.sh
@@ -1,4 +1,4 @@
 echo -e "\n\t\t\t\tPUSH-TO-GIT\n\n"
 git add . ;
 echo -e "\n\t>> Please enter commit message followed by enter :" && read COMMITMSG && git commit -m "$COMMITMSG" && echo -e "\n\t>>Commit ran Successfully!\n\t\t>> Should I continue? <click-return>" && read && clear;
-git push && echo -e "\n\t>> Push command Done! click return to clear screen." && read && clear
+echo && git push && echo -e "\n\t>> Push command Done! click return to clear screen." && read && clear

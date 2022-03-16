607619d045af0f6daad98ea0e0e23ed0fa82c0ed
Thonifho Muhali <tmuhali@student.wethinkcode.co.za>
2022-03-16 19:36:45 +0000

    hblkvf

--- dev_scripts/git_utils/push.sh
+++ dev_scripts/git_utils/push.sh
@@ -1,3 +1,3 @@
 git add . ;
-echo -e "\n\tPlease enter commit message :" && read COMMITMSG && git commit -m "$COMMITMSG" && echo "Commited Successfully! press return." && read && clear;
-git push && echo -e "\n\tPush command Done! click return to clear screen." && read && clear
+echo -e "\n\t>> Please enter commit message :" && read COMMITMSG && git commit -m "$COMMITMSG" && echo -e "\n\t>>Commit ran Successfully! press return." && read && clear;
+git push && echo -e "\n\t>> Push command Done! click return to clear screen." && read && clear

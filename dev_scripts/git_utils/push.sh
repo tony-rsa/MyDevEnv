3870c8ca0c2ea2f6c273676a166437e60e46264f
Thonifho Muhali <tmuhali@student.wethinkcode.co.za>
2022-03-16 19:30:11 +0000

    sd

---
+++ dev_scripts/git_utils/push.sh
@@ -0,0 +1,1 @@
+git add . && echo -e "\tPlease enter commit message :" && read COMMITMSG && git commit -m $COMMITMSG && git push && echo -e "Push command Done! click return to clear screen." && read && clear
